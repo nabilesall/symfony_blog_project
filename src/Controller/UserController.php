d2a942b49c46c6a0b3ed19424dee3addcdba7ff5
nabilesall <nabilesall@gmail.com>
2023-04-02 21:57:56 +0200

    hash password

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\FormError;
 
 class UserController extends AbstractController
 {
@@ -19,6 +20,8 @@ class UserController extends AbstractController
      */
     public function connection(Request $request, ManagerRegistry $doctrine): Response
     {
+
+        echo password_hash("ad", PASSWORD_DEFAULT, ['cost' => 12]);;
 
         $userForConnection = new User();
         $form = $this->createForm(ConnectionType::class, $userForConnection);
@@ -31,13 +34,19 @@ class UserController extends AbstractController
             $userRepository = $doctrine->getRepository(User::class);
             $user = $userRepository->findOneBy([
                 'UserName' => $userForConnection->getUserName(),
-                'UserPassword' => $userForConnection->getUserPassword()
+                //'UserPassword' => $userForConnection->getUserPassword()
             ]);
 
-            if ($user) {
+            if ($user && password_verify($userForConnection->getUserPassword(), $user->getUserPassword())) {
                 $request->getSession()->set('userName', $user->getUserName());
                 $request->getSession()->set('userStatus', $user->getUserStatus());
                 return $this->redirectToRoute('home');                
+            }else{
+                $form ->addError(new FormError('Nom d\'utilisateur ou mot de passe incorrect'));
+                return $this->render('connection.html.twig', [
+                    'form' => $form->createView(),
+                    'error' => 'Nom d\'utilisateur ou mot de passe incorrect'
+                ]);
             }
         }
 
@@ -61,15 +70,31 @@ class UserController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $userForInscription = $form->getData();
 
-            $userRepository = $doctrine->getRepository(User::class);
-            $userRepository -> save($userForInscription,true);
+            $exitUserName = $doctrine->getRepository(User::class)->findOneBy([
+                'UserName' => $userForInscription->getUserName()
+            ]);
 
-            return $this->redirectToRoute('connection');
+            if($exitUserName) {
+                $form ->addError(new FormError('Ce nom d\'utilisateur est déjà utilisé'));
+                return $this->render('inscription.html.twig', [
+                    'form' => $form->createView()
+                ]);
+            }
+            else{
+                // Hashing the password
+                $plainPassword = $doctrine->getRepository(User::class)->getUserPassword();
+                $hashedPassword = password_hash($plainPassword, PASSWORD_DEFAULT, ['cost' => 12]);
+                $userForInscription->setUserPassword($hashedPassword);
+
+                $userRepository -> save($userForInscription,true);
+
+                return $this->redirectToRoute('connection');
+            }
+            
         }
 
         return $this->render('inscription.html.twig', [
             'form' => $form->createView(),
-            'controller_name' => 'InscriptionController',
         ]);
     }
 
