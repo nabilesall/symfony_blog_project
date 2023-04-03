f39c827d568a9fbc96ae3814b12641b0ce4e2812
nabilesall <nabilesall@gmail.com>
2023-04-03 21:21:07 +0200

    liens vers catégories
    afficher la liste des postes pour une catégorie

--- src/Controller/User/CategoryController.php
+++ src/Controller/User/CategoryController.php
@@ -1,20 +1,46 @@
 <?php
 
-// Path: src\Controller\User\CategoryController.php
 namespace App\Controller\User;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Persistence\ManagerRegistry;
 
 class CategoryController extends AbstractController
 {
     /**
-     * @Route("/user/category", name="user_category")
+     * @Route("/user/category/{id}", name="user.category.show")
      */
-    public function index()
+    public function show(Request $request, ManagerRegistry $doctrine, $id): Response
     {
-        return $this->render('user/category/index.html.twig', [
-            'controller_name' => 'CategoryController',
+        $categoryRepository = $doctrine->getRepository(\App\Entity\Category::class);
+        $category = $categoryRepository->find($id);
+
+        $categoryInArray = array(
+            "id" => $category->getId(),
+            "name" => $category->getName(),
+        );
+
+        $posts = $category->getPosts();
+
+        $postsInArray = array();
+
+        foreach($posts as $post){
+            $postInArray = array(
+                "id" => $post->getId(),
+                "title" => $post->getTitle(),
+                "content" => \App\Utils\Text::excerpt($post->getContent()),
+                "publishedAt" => $post->getPublishedAt(),
+            );
+            $postsInArray[] = $postInArray;
+        }
+
+        return $this->render('user/category/show.html.twig', [
+            'category' => $categoryInArray,
+            'posts' => $postsInArray,
+            'userName' => $request->getSession()->get('userName'),
         ]);
     }
 }
